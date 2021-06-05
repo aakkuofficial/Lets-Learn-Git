fe39bfdd11a998960c4dabb5ebb17677b6ffe34c
Aakash kumar <aakku031998@gmail.com>
2021-06-06 02:07:54 +0530

    initial commit

---
+++ ki.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a,b,sum;
+    printf("Enter Any 2 Numbers");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("Ans is=%d",sum);
+    getch();
+}
