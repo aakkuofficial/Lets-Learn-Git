61c4b5eb465975f9399266bcac824a92c97be002
Aakash kumar <aakku031998@gmail.com>
2021-06-06 02:28:19 +0530

    added multiply.c

---
+++ multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int s,d,mul;
+    printf("Enter Any 2 Numbers");
+    scanf("%d %d",&s,&d);
+    mul=s*d;
+    printf("%d",mul);
+    getch();
+}
